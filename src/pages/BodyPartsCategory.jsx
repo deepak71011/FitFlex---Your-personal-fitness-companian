85ec0bd03c04fb0e98e9257bbed70e67cdf2002e
deepak71011 <sdeepak71011@gmail.com>
2025-09-17 19:21:46 +0530

    Update BodyPartsCategory.jsx

--- src/pages/BodyPartsCategory.jsx
+++ src/pages/BodyPartsCategory.jsx
@@ -23,7 +23,7 @@ const BodyPartsCategory = () => {
             url: `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${id}`,
             params: {limit: '200'},
             headers: {
-              'X-RapidAPI-Key': 'ae40549393msh0c35372c617b281p103ddcjsn0f4a9ee43ff0',
+              'X-RapidAPI-Key': '4becbb1a13mshaee39edab9fb568p1a98f7jsn6187be9a0837',
               'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
             }
         };
@@ -68,5 +68,6 @@ const BodyPartsCategory = () => {
         </div>
   )
 }
+
 
 export default BodyPartsCategory
